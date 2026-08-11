8bbe84d0fd318ab37c4096e8eaed407185dd8ecb
TheRoamersCult <info@theroamerscult.com>
2026-08-11 19:32:23 +0530

    Direct fix for Supabase credentials

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -14,14 +14,23 @@
 
 
 
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// console.log("SUPABASE URL CHECK:", supabaseUrl); 
+
+// export const supabase = createClient(
+//   supabaseUrl || '', 
+//   supabaseAnonKey || ''
+// );
+
+
+
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://jscpbyvmtvoqtsrmefth.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzY3BieXZtdHZvcXRzcm1lZnRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNjczNjAsImV4cCI6MjA5ODY0MzM2MH0.5urieh_vJQ06yC4k0fydujSwxXkUO9Etw-BxLe_vCRM" ;
 
-console.log("SUPABASE URL CHECK:", supabaseUrl); // F12 console mein yahan URL print hoga
-
-export const supabase = createClient(
-  supabaseUrl || '', 
-  supabaseAnonKey || ''
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
