ce78bcc99a39ec9fe7fa6012d2a03453921bb44c
TheRoamersCult <info@theroamerscult.com>
2026-08-11 19:35:34 +0530

    Force update supabase credentials

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -33,4 +33,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://jscpbyvmtvoqtsrmefth.supabase.co";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzY3BieXZtdHZvcXRzcm1lZnRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNjczNjAsImV4cCI6MjA5ODY0MzM2MH0.5urieh_vJQ06yC4k0fydujSwxXkUO9Etw-BxLe_vCRM" ;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey) ;
